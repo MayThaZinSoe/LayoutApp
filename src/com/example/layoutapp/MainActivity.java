a6010175a2b3a1bb6030190268604c08a7769034
MayThaZinSoe <tzsakura@gmail.com>
2014-08-17 18:51:33 +0900

    Only Location Button

--- src/com/example/layoutapp/MainActivity.java
+++ src/com/example/layoutapp/MainActivity.java
@@ -1,7 +1,27 @@
 package com.example.layoutapp;
 
+import java.net.HttpURLConnection;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -15,6 +35,8 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+	
+		private LocationManager locationManager = null;	
 	Context context;
 
 	TextView textView ;
@@ -25,9 +47,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
     	context = this;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         
-        Button button = (Button)findViewById(R.id.button1);
+        setContentView(R.layout.activity_main);
+      
+		
+		Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(mButton1Listener);
+        
+   /*     Button button = (Button)findViewById(R.id.button1);
         button.setOnClickListener(new OnClickListener(){
        
         	@Override
@@ -56,8 +83,28 @@ public class MainActivity extends Activity {
         	}
         });
         
-       
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com/"));
+        			startActivity(intent); 
+        	}
+        });*/
         
+    /* Button button4 = (Button)findViewById(R.id.button4);
+        button4.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		Uri uri = Uri.parse("mailto:mzkrelx@gmail.com"); 
+        		Intent intent = new Intent(Intent.ACTION_SENDTO, uri); 
+        		intent.putExtra(Intent.EXTRA_SUBJECT, "I am May"); 
+        		intent.putExtra(Intent.EXTRA_TEXT, "konichiha"); 
+        		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+        		startActivity(intent); 
+        	}
+        });
+       */
     }
 
 
@@ -79,4 +126,132 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-}
+    
+
+
+
+
+
+	private OnClickListener mButton1Listener = new OnClickListener() {
+		public void onClick(View v) {
+	        if (locationManager != null) {
+	        	// 取得処理を終了
+	        	locationManager.removeUpdates(mLocationListener);
+	        }
+        	locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        	
+            // GPSから位置情報を取得する設定
+            boolean isGpsOn = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        	// 3Gまたはwifiから位置情報を取得する設定
+            boolean isWifiOn =  locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            String provider = null;
+			if (isGpsOn) {
+				provider = LocationManager.GPS_PROVIDER;
+			} else if (isWifiOn) {
+            	provider = LocationManager.NETWORK_PROVIDER;
+            } else {
+            	Toast.makeText(getApplicationContext(), "Wi-FiかGPSをONにしてください", Toast.LENGTH_LONG).show();
+            	return;
+            }
+			Toast.makeText(getApplicationContext(), "Provider=" + provider, Toast.LENGTH_LONG).show();
+			
+			// ロケーション取得を開始
+            locationManager.requestLocationUpdates(provider, 1000L, 0, mLocationListener);
+        }
+	};
+
+	private LocationListener mLocationListener = new LocationListener() {
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+        public void onProviderEnabled(String provider) {
+        }
+        public void onProviderDisabled(String provider) {
+        }
+        public void onLocationChanged(Location location) {
+        	String latitude = Double.toString(location.getLatitude());
+        	String longitude = Double.toString(location.getLongitude());
+        	String message = "";
+            message += ("緯度："+latitude);
+            message += "\n";
+            message += ("経度："+longitude);
+            message += "\n";
+            message += ("Accuracy"+Float.toString(location.getAccuracy()));
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            
+            // 位置情報の取得を1回しか行わないので取得をストップ
+            locationManager.removeUpdates(mLocationListener);
+
+//            showYahooMap(latitude, longitude);
+            mailYahooMap(latitude, longitude);
+//            getRequest(latitude, longitude);
+        }
+    };
+    
+    @Override
+    protected void onPause() {
+        if (locationManager != null) {
+        	locationManager.removeUpdates(mLocationListener);
+        }
+        super.onPause();
+    }
+
+	private void showYahooMap(String latitude, String longitude) {
+		String urlString = "http://map.yahoo.co.jp/maps?type=scroll&pointer=on&sc=2"
+				+ "&lat=" + latitude
+				+ "&lon=" + longitude;
+
+		// 地図をブラウザでみる
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlString));
+		startActivity(intent);
+	}
+	
+	private void mailYahooMap(String latitude, String longitude) {
+		String urlString = "http://map.yahoo.co.jp/maps?type=scroll&pointer=on&sc=2"
+				+ "&lat=" + latitude
+				+ "&lon=" + longitude;
+
+		
+    	
+    	
+	}
+
+	private	void getRequest(String latitude, String longitude) {
+		// お天気
+		// http://openweathermap.org/
+		String requestURL = "http://api.openweathermap.org/data/2.5/forecast/daily"
+				+ "?lat=" + latitude + "&lon=" + longitude
+				+ "&xmode=json&cnt=1";
+		// 逆ジオコーディングサービス
+		// http://www.finds.jp/wsdocs/rgeocode/index.html.ja
+//		String requestURL = "http://www.finds.jp/ws/rgeocode.php?json&lat=" + latitude + "&lon=" + longitude;
+		Task task = new Task();
+        task.execute(requestURL);
+	}
+
+	protected class Task extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... params)
+        {
+            HttpClient client = new DefaultHttpClient();
+            HttpGet get = new HttpGet(params[0]);
+            byte[] result = null;
+            String rtn = "";
+            try {
+                HttpResponse response = client.execute(get);
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpURLConnection.HTTP_OK){
+                    result = EntityUtils.toByteArray(response.getEntity());
+                    rtn = new String(result, "UTF-8");
+                }
+            } catch (Exception e) {
+            }
+            return rtn;
+        }
+        
+      
+
+            }
+           
+    }
+
+
